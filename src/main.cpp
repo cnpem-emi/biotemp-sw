69c1e2f68070ca31ff9499178b9c667e8c6b61ec
Rodrigo da Silva Moraes <rodrigo.moraes@cnpem.br>
2024-05-02 11:11:26 -0300

    Connected to HiveMQ broker

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@ WiFiConnection wifi;
 void setup() {
   Serial.begin(9600);
   wifi.config();
+  wifi.configMQTT();
 
 }
 
 void loop() {
+  wifi.mqttLoop();
+  wifi.publishMessage("test", "Hello World", true);
   Serial.println(wifi.getIP());
   delay(1000);
 }
