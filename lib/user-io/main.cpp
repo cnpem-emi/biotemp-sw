252fc3c0791d9a6ff9b57df7b92af6995fd6fe51
pedro.trindade <pedro.trindade@fieb.org.br>
2024-05-07 23:41:55 -0300

    chore: added display from other branch

--- lib/user-io/main.cpp
+++ lib/user-io/main.cpp
@@ -1,32 +1,29 @@
-#include <Arduino.h>
-#include <InputController.hpp>
 
-#include "temphandler.hpp"
 #include "display.hpp"
 
 Display disp;
+
 std::vector<std::string> menuList = {"Menu 1", "Menu 2", "Menu 3"};
 
-InputController encoder;
+
 
 void setup() {
-  encoder.config(2);
-  TempHandler temp_handler; 
   Serial.begin(9600);
   disp.displayConfig();
+
   pinMode(UP_BUTTON_PIN, INPUT);
   pinMode(DOWN_BUTTON_PIN, INPUT);
   pinMode(SELECT_BUTTON_PIN, INPUT);
 }
 
 void loop() {
-
   disp.showMenuTitle("Menu Principal");
   disp.createMenu(menuList);
   disp.upButton();
   disp.downButton(); 
   disp.drawArrow(disp.arrowPos);
   Serial.println(disp.arrowPos);
-  delay(100);  
-
+  delay(100);                                       
+  
 }
+
