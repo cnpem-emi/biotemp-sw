ed9d3e0e961b70b55546bc89ca5e81e7632ddb91
Pedro-Mamud <pedro.mamud@cnpem.br>
2024-05-08 14:00:26 -0300

    initial commit

---
+++ include/graphicalViewHandler.hpp
@@ -0,0 +1,17 @@
+#ifndef _INCLUDE_GRAPHICALVIEWHANDLER_HPP_
+#define _INCLUDE_GRAPHICALVIEWHANDLER_HPP_
+
+class graphicalViewHandler
+{   
+    public:
+        void 
+        
+        
+
+    private:    
+    
+};
+
+
+
+#endif // _INCLUDE_GRAPHICALVIEWHANDLER_HPP_
