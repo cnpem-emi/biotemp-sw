ba4d0af1ad1ed344b04e62a9418898680478e0ee
Pedro Trindade <pedrohenriqueatrindade@gmail.com>
2024-07-26 15:35:10 -0300

    feat: added customization to doxygen output

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/*! @mainpage Biotemp Temperature Monitoring System
+ *
+ * @section intro_sec Introduction
+ *
+ *  The Biotemp Instrumentation System is a user-friendly IoT device designed for collecting room temperature data.
+ *  It is made to be effortless to set up and use, in a plug-and-play manner. Equipped with menus displayed on an LCD screen,
+ *  it offers a straightforward interface for users to access and manage temperature information effectively.  
+ */
+
 #include "biotemp.hpp"
 
 Biotemp biotemp;
